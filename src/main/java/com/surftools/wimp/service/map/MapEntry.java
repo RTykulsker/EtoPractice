929209a1b98e6e18e1b9f686aa4a1fbdad3a6763
Bob Tykulsker <RTykulsker@users.noreply.github.com>
2025-12-27 11:50:29 -0800

    Improve popup on feedback count map
    should be back-ported to wimp

--- src/main/java/com/surftools/wimp/service/map/MapEntry.java
+++ src/main/java/com/surftools/wimp/service/map/MapEntry.java
@@ -63,7 +63,9 @@ public record MapEntry(String label, String to, LatLongPair location, String mes
     var messageId = feedbackMessage.message().messageId;
     var count = feedbackResult.feedbackCount();
     var color = gradientMap.getOrDefault(count, lastColor);
-    var content = "MessageId: " + messageId + "\n" + "Feedback Count: " + feedbackResult.feedbackCount() + "\n"
+    var from = feedbackMessage.message().from;
+    var prefix = "<b>" + from + "</b><hr>";
+    var content = prefix + "MessageId: " + messageId + "\n" + "Feedback Count: " + feedbackResult.feedbackCount() + "\n"
         + "Feedback: " + feedbackResult.feedback();
     return new MapEntry(feedbackResult.call(), to, location, content, color);
   }
